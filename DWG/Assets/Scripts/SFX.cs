71ee0dff656d3ec2a56486344005abf3ef927e5a
Brian Walsh <bwalsh524@knights.ucf.edu>
2016-03-23 10:20:27 -0400

    Sound Test

---
+++ DWG/Assets/Scripts/SFX.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SFX : MonoBehaviour {
+
+	private List<AudioSource> source = new List<AudioSource>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (source.Count > 0) {
+		
+			foreach (AudioSource s in source) {
+		
+				if (!s.isPlaying) {
+
+					Destroy (s);
+
+				}
+		
+			}
+
+		}
+
+	}
+
+	public void PlaySound(AudioClip clip){
+
+		AudioSource newSource = gameObject.AddComponent<AudioSource>();
+
+		source.Add (newSource);
+
+		newSource.clip = clip;
+		newSource.Play ();
+
+	}
+
+}
---
+++ DWG/Assets/SoundTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+		
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)){
+
+				if(hit.collider.gameObject.name = "SoundTest"){
+
+
+
+				}
+
+			}
+		
+		}
+	
+	}
+}
